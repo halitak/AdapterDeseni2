94a4756e5a078132e77d93fee507fc7ec523c4d8
Halit Ak <halitak63@gmail.com>
2017-06-12 12:38:16 +0300

    design patterns
    in c#

---
+++ AdapterDeseni2/Adaptee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDeseni2
+{
+    class Adaptee
+    {
+        public virtual void KemenceSiparisi() {
+            Console.WriteLine("Kemençe siparişi verilmiştir");
+        }
+    }
+}
---
+++ AdapterDeseni2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDeseni2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target target = new Target();
+            target.Siparis();
+
+            Adapter adapter = new Adapter();
+            adapter.Siparis();
+
+            Console.ReadKey();
+        }
+    }
+}
